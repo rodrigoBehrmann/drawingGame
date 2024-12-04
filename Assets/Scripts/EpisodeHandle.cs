3ef649a8f8cae907084bf4a9b81d558ad8d80908
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2024-12-04 16:34:24 -0300

    fix bugs and add audios

--- Assets/Scripts/EpisodeHandle.cs
+++ Assets/Scripts/EpisodeHandle.cs
@@ -7,7 +7,10 @@ using TMPro;
 public class EpisodeHandle : MonoBehaviour
 {
     [Header("Episode Data")]
-    [SerializeField] private Episode _episode;    
+    [SerializeField] private Episode _episode; 
+
+    [Header("Episode Sound Effect")]   
+    [SerializeField] private AudioSource _audioSource;
 
     [Header("Forward Button - IF NO OPTIONS")]
     [SerializeField] private TextMeshProUGUI _dialogueText;
@@ -21,77 +24,105 @@ public class EpisodeHandle : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _option2ButtonText;
 
     [Header("Option Panels - IF HAVE OPTIONS")]
-    [SerializeField] private GameObject _option1_Episode;
-    [SerializeField] private GameObject _option2_Episode;
+    [SerializeField] private GameObject _option1Episode;
+    [SerializeField] private GameObject _option2Episode;
 
 
+    // Called at the start of the game to initialize episode behavior
     void Start()
+    {
+        InitializeEpisodeUI();
+    }
+
+    // Initializes the UI elements based on whether there are options or not
+    private void InitializeEpisodeUI()
     {
         if (_episode.HaveOptions)
         {
-            _option1Button.gameObject.SetActive(true);
-            
-            _option2Button.gameObject.SetActive(true);
-            
-            _option1ButtonText.text = _episode.Option1;
-
-            _option2ButtonText.text = _episode.Option2;
-
-            _forwardButton.gameObject.SetActive(false);
-
-            _dialogueText.gameObject.SetActive(false);
+            SetUpOptionButtons();
         }
         else
         {
-            _option1Button.gameObject.SetActive(false);
-            _option2Button.gameObject.SetActive(false);
-            
-            if(_episode.IsFinalEpisode)
-            {
-                _forwardButton.onClick.AddListener(() =>
-                {
-                    _nextEpisode.SetActive(true);
-                    gameObject.SetActive(false);
-                });
-            }else
-            {
-                _forwardButton.onClick.AddListener(() =>
-                {
-                    _nextEpisode.SetActive(true);
-                    gameObject.SetActive(false);
-                });
-            }
-
-            _dialogueText.text = _episode.Dialogue;
+            SetUpForwardButton();
         }
 
-        _option1Button.onClick.AddListener(() =>
+        SetOptionButtonListeners();
+
+        // Hide the episode initially if it's not the first episode
+        if (_episode.EpisodeNumber != 1)
         {
             gameObject.SetActive(false);
-            _option1_Episode.SetActive(true);
-            _option2_Episode.SetActive(false);
+        }
+
+        // Set the episode image
+        GetComponent<Image>().sprite = _episode.Image;
+    }
+
+    // Sets up the UI for episodes with options
+    private void SetUpOptionButtons()
+    {
+        _option1Button.gameObject.SetActive(true);
+        _option2Button.gameObject.SetActive(true);
+        _option1ButtonText.text = _episode.Option1;
+        _option2ButtonText.text = _episode.Option2;
+        _forwardButton.gameObject.SetActive(false);
+        _dialogueText.gameObject.SetActive(false);
+    }
+
+    // Sets up the UI for episodes without options
+    private void SetUpForwardButton()
+    {
+        _option1Button.gameObject.SetActive(false);
+        _option2Button.gameObject.SetActive(false);
+        _forwardButton.onClick.AddListener(OnForwardButtonClick);
+        _dialogueText.text = _episode.Dialogue;
+    }
+
+    // Sets listeners for the option buttons
+    private void SetOptionButtonListeners()
+    {
+        _option1Button.onClick.AddListener(() =>
+        {
+            SwitchToOption1Episode();
         });
 
         _option2Button.onClick.AddListener(() =>
         {
-            gameObject.SetActive(false);
-            _option1_Episode.SetActive(false);
-            _option2_Episode.SetActive(true);
+            SwitchToOption2Episode();
         });
-
-        if(_episode.EpisodeNumber != 1)
-        {
-            gameObject.SetActive(false);
-        }
-
-        
-
-        GetComponent<Image>().sprite = _episode.Image;
     }
 
-    public void SetEpisode(Episode episode)
+    // Handles forward button click (for episodes without options)
+    private void OnForwardButtonClick()
     {
-        _episode = episode;
+        _nextEpisode.SetActive(true);
+        _audioSource.Stop();
+        gameObject.SetActive(false);
+    }
+
+    // Switches to the episode for option 1
+    private void SwitchToOption1Episode()
+    {
+        gameObject.SetActive(false);
+        _option1Episode.SetActive(true);
+        _option2Episode.SetActive(false);
+    }
+
+    // Switches to the episode for option 2
+    private void SwitchToOption2Episode()
+    {
+        gameObject.SetActive(false);
+        _option1Episode.SetActive(false);
+        _option2Episode.SetActive(true);
+    }
+
+    // Called when the episode is enabled to play the sound effect
+    private void OnEnable() 
+    {
+        if (_episode == null) return;
+
+        _audioSource.clip = _episode.SoundEffect;
+        _audioSource.Play();
     }
 
 }
